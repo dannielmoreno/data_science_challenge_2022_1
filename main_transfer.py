0eef49ac512f914234af0975ae3488d777dfebe4
dannielmoreno <dr.moreno@uniandes.edu.co>
2022-01-26 07:37:17 +0000

    main_transfer.py code commented

--- main_transfer.py
+++ main_transfer.py
@@ -15,6 +15,7 @@ import pandas as pd
 from tqdm import tqdm
 import pickle
 
+# Train and test dataset classes for loading the data to the PyTorch model easily
 
 class TrainTrafficDataset(Dataset):
     def __init__(self, datapath, transform):
@@ -45,7 +46,7 @@ class TestTrafficDataset(Dataset):
         class_id = int(img_path.split('/')[2].split('_')[0])
         return image, class_id
 
-
+# Function that evaluates a model accuracy in train and test set
 
 def evaluate(model, trainloader, testloader, device):
     correct_train, total_train, correct_test, total_test = 0, 0, 0, 0
@@ -69,10 +70,17 @@ def evaluate(model, trainloader, testloader, device):
     test_accuracy = correct_test * 100 / total_test
     return train_accuracy, test_accuracy
 
+# Function associated to training a model for a given number of epochs and evaluating the model performance during
+# each epoch
+
 def train(model, criterion, optimizer, epochs, trainloader, testloader, device, savepath=None):
+
+    # Initializes lists that will record progress throughout training
     loss_list = []
     train_acc_list = []
     test_acc_list = []
+
+    # Initializes the best accuracies in test set during this training and among all the previously trained models
     best_acc = 0
     try:
         with open('best_test_accuracy.pkl', 'rb') as file:
@@ -80,6 +88,7 @@ def train(model, criterion, optimizer, epochs, trainloader, testloader, device, 
     except:
         best_global_acc = 0
 
+    # Training during the indicated number of epochs
     for epoch in range(epochs):
         running_loss = 0.0
         model.train()
@@ -99,9 +108,11 @@ def train(model, criterion, optimizer, epochs, trainloader, testloader, device, 
         train_acc_list.append(train_accuracy)
         test_acc_list.append(test_accuracy)
 
+    # Saves the best performance of this training with the model and experiment id
         if savepath is not None and test_acc_list[-1] > best_acc:
             best_acc = test_acc_list[-1]
             torch.save(model.state_dict(), savepath)
+    # If this model achieves the overall best performance among models it is saved as "best_model.pth"
         if best_acc > best_global_acc:
             best_global_acc = best_acc
             with open('best_test_accuracy.pkl', 'wb') as file:
@@ -109,6 +120,8 @@ def train(model, criterion, optimizer, epochs, trainloader, testloader, device, 
             torch.save(model.state_dict(), os.path.join("models", "best_model.pth"))
 
     return model, loss_list, train_acc_list, test_acc_list
+
+# Function that creates and saves loss and accuracy graphs for training and test sets
 
 def result_visualization(epochs, loss_list, train_acc_list, test_acc_list, model_id, experiment_id, save=False):
 
@@ -134,13 +147,18 @@ def result_visualization(epochs, loss_list, train_acc_list, test_acc_list, model
         plt.savefig(os.path.join("results", "acc_" + str(model_id) + "_" + str(experiment_id) + ".png"))
     plt.show()
 
-def main(epochs, batch_size, model_id, experiment_id, transfer):
+# Main Function
 
+def main(epochs, lr, batch_size, model_id, experiment_id, transfer):
+
+    # Statement that allows employing GPUs
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
     train_datapath = os.path.join("traffic_Data", "DATA")
     test_datapath = os.path.join("traffic_Data", "TEST")
 
+    # This transformations fix the model input size of the image to 128x128, and normalizes its intensity according to
+    # the training set distribution. It also applies data augmentation
     tsfm_train = transforms.Compose([
         transforms.Resize((128, 128)),
         transforms.Normalize(mean=[0.485, 0.456, 0.406],
@@ -150,6 +168,7 @@ def main(epochs, batch_size, model_id, experiment_id, transfer):
         transforms.RandomPerspective()
 
     ])
+    # The test set dataloader does not apply any data augmentation transformation
     tsfm_test = transforms.Compose([
         transforms.Resize((128, 128)),
         transforms.Normalize(mean=[0.485, 0.456, 0.406],
@@ -161,14 +180,21 @@ def main(epochs, batch_size, model_id, experiment_id, transfer):
 
     trainloader = DataLoader(trainset, batch_size=batch_size, shuffle=True)
     testloader = DataLoader(testset, batch_size=batch_size, shuffle=False)
+
+    # This first model parts form a pretrained AlexNet and modifies the output size of the last linear layer in order
+    # to adjust to the 58 classes of the dataset. During training all parameters are subject to backpropagation.
     if transfer == "fine_tune":
         model = models.alexnet(pretrained=True)
         model.classifier = nn.Linear(9216, 58)
+    # This second model employs the same pretrained AlexNet but just the last layer is subject to backpropagation, the
+    # rest of the parameters are fixed.
     elif transfer == "fixed":
         model = models.alexnet(pretrained=True)
         for param in model.parameters():
             param.requires_grad = False
             model.classifier = nn.Linear(9216, 58)
+    # This third model employs a pretrained resnet whose parameters are fixed to extract image embeddings, and then
+    # employs an MLP as classifier.
     elif transfer == "resnet":
         model = models.resnet34(pretrained=True)
         for param in model.parameters():
@@ -184,6 +210,7 @@ def main(epochs, batch_size, model_id, experiment_id, transfer):
             nn.Linear(256, 58)
         )
 
+    # Sends the model to GPU if available.
     model.to(device)
 
     """ For assigning weights to the loss associated to each class according to the inverse of its frequency """
@@ -199,7 +226,7 @@ def main(epochs, batch_size, model_id, experiment_id, transfer):
 
 
     criterion = nn.CrossEntropyLoss()
-    optimizer = optim.SGD(model.parameters(), lr=0.0007, momentum=0.9)
+    optimizer = optim.SGD(model.parameters(), lr=lr, momentum=0.9)
 
     model_savepath = os.path.join("models", "model_"+str(model_id)+"_"+str(experiment_id)+".pth")
 
@@ -208,4 +235,13 @@ def main(epochs, batch_size, model_id, experiment_id, transfer):
 
     result_visualization(epochs, loss_list, train_acc_list, test_acc_list, model_id, experiment_id, save=True)
 
-main(epochs=20, batch_size=10, model_id=12, experiment_id=6, transfer="fine_tune")
+# Change for each experiment
+
+epochs = None
+lr = None
+batch_size = None
+model_id = None
+experiment_id = None
+transfer = None
+
+main(epochs=epochs, lr=lr, batch_size=batch_size, model_id=model_id, experiment_id=experiment_id, transfer=transfer)
